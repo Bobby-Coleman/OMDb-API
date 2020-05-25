1cfc8000dcc37e7bcbddb17a4b8e72c8c101e47c
Bobby Coleman <colemankidding@gmail.com>
2020-05-25 12:35:38 -0700

    add onChange to search input

--- src/omdb/omdb-search.js
+++ src/omdb/omdb-search.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
 
 class OmdbSearch extends Component {
+
+    state= {
+        keyword: ''
+    }
+
+    keywordChanged = event => {
+        console.log(event.target.value)
+    }
+
+
     render() {
         return (
            <> 
             <h1>Ombd Search</h1>
             <div>
-                <input className="omdb-search" placeholder="Enter a Movie Title"/>
+                <input 
+                value={this.state.keyword} 
+                className="omdb-search" 
+                onChange={this.keywordChanged}
+                placeholder="Enter a Movie Title"
+                />
                 <button>Search</button>
             </div>
            </>
